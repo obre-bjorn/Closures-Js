853300958e74bb3109978fcdc747aa818045b7d1
obre-bjorn <obre.bjorn@gmail.com>
2023-02-17 09:41:00 +0300

    function binding

--- closure.js
+++ closure.js
@@ -2,13 +2,14 @@
 
 let number = { num: 3 }
 
-let adder = function(a) {
-    return this.num + a
+let adder = function(a, b) {
+    return this.num + a * b
 }
 
 
+// Functionname.call(object, function arguments)
 
-let result = adder.call(3, number);
+let result = adder.call(number, 4, 3);
 
 
 console.log('====================================');
@@ -17,4 +18,36 @@ console.log('====================================');
 
 
 
-// Bind Function
+// Apply Function
+
+let person = {
+    name: 'Bjorn',
+    age: 23
+}
+
+let info = function(years) {
+    return `${this.name} will be ${this.age + years} in the next ${years} years.
+    `
+}
+
+// Replaces function arguments with and array of Arguments.
+// functionname.apply(scope/object, [Array of Arguments])
+console.log(info.apply(person, [4]));
+
+
+// Bind function
+
+let book = {
+    title: 'Way of Men',
+    pages: 239
+}
+
+
+function getBookPages() {
+    return `${this.title} contains ${this.pages} pages`
+}
+
+//  Does not invoke/call the function immediately rather it returns a function to be executed later in the code
+// function parameters can be passed later on either in function arguments or array of arguments.
+let getBookInfo = getBookPages.bind(book)
+console.log(getBookInfo())
